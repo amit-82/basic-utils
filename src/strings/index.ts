a940b238e34af5a382782958138e597496e4e255
amit.leshem <amit.leshem@agritask.com>
2021-10-14 21:55:46 +0300

    added defaultFormattedNumber

--- src/strings/index.ts
+++ src/strings/index.ts
@@ -1,10 +1,11 @@
 import { convertStringToNumberAndDivideBy100 } from './convertToNumber';
-import { formattedNumber, formattedNumberFactory, defaultFormattedNumber } from './prettyNumber';
+import { defaultFormattedNumber, formattedNumber, formattedNumberFactory } from './prettyNumber';
 import { nonNumeric, nonNumericMinusDot } from './regs';
 import { converNamedMonthDate, rearrangeFromAmericanFormat } from './stringToDate';
 
 export {
   convertStringToNumberAndDivideBy100,
+  defaultFormattedNumber,
   formattedNumber,
   formattedNumberFactory,
   nonNumeric,
