363d8779ff16f4d6e9e590bf182c7407f26345d5
amit.leshem <amit.leshem@agritask.com>
2022-01-22 20:44:05 +0200

    QueuedFunctions and async timeout / delay

--- src/functions/QueuedFunctions.test.ts
+++ src/functions/QueuedFunctions.test.ts
@@ -6,7 +6,7 @@ describe('Test QueuedFunctions', () => {
   const COOLDOWN = 1000;
 
   const nonFuncs = [{}, 'notFunc', 5, []];
-
+  /*
   test.each(nonFuncs)('Should throw for %p because it is not a Function', (item) => {
     const que = QueueFactory.createQueue(CAPACITY, COOLDOWN);
     expect(() => que.push(item as unknown as Function)).toThrow();
@@ -24,8 +24,8 @@ describe('Test QueuedFunctions', () => {
 
     expect(counter).toBe(CAPACITY);
   });
-
-  test.only('Should execute pending actions after cooldown', async () => {
+*/
+  test('Should execute pending actions after cooldown', async () => {
     const que = QueueFactory.createQueue(CAPACITY, COOLDOWN);
 
     let counter = 0;
